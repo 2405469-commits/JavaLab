525b5bbd99e02a521e558cad4f9d65e48cd25c33
GameZ0n <swapnil0roy@gmail.com>
2025-12-17 23:31:00 +0530

    Final

---
+++ Lab2/AreaOfRec.java
@@ -0,0 +1,31 @@
+package Lab2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AreaOfRec {
+    double l,b,area;
+    AreaOfRec()
+    {
+        l=0;
+        b=0;
+        area=0;
+    }
+    AreaOfRec(double len,double bre)
+    {
+        l=len;
+        b=bre;
+        area=l*b;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Length:");
+        double l=sc.nextDouble();
+        System.out.print("Enter Length:");
+        double b=sc.nextDouble();
+        AreaOfRec o1=new AreaOfRec(l,b);
+        System.out.println("Length:"+l+"\nBreadth:"+b+"\nArea:"+o1.area);
+    }
+}
